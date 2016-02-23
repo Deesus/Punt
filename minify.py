1a39eea8225ebdf7f654df9ba5b87479e9dbc867
Dee Reddy <d.reddy@yandex.com>
2016-02-23 00:52:58 -0500

    feat: Update regex. Add file write-out.
    Regex is bugged, however. Need to fix regex pattern.

--- minify.py
+++ minify.py
@@ -8,36 +8,49 @@ import re
 args = sys.argv[1:]
 
 
-def minify(filepath, comments=False):
+def minify(input_path, output_path, comments=False):
     """ Minifies/uglifies file
-    :param
-        file_:
+    args:
+        input_path: input file path
+        output_path: write-out file path
         comments: Boolean. If False, deletes comments during output.
-    :return:
+    returns:
         Minified string.
+    example:
+        `$ python minify.py ./src/styles.css ./src/output.css`
     """
 
     pattern = re.compile(r"""
-        \s |                    # matches all whitespace characters OR
-         (                      #
-            /\*                 # /*
-            [
-                \w\s
-                (?=\*)
-                :@!"'~\.\^\$\+\?\{\}\[\]\\\|\(\)
-            ]*                  # AND
-            \*/                 # */
-         )                      #
-         | //.*\n               # OR any character from // until end-line (inclusive)
+    \s |                                # matches all whitespace characters OR
+     (                                  #
+        /\*                             # /* AND
+        [                               # 0 or more of any character
+            \w\s                        #
+            (?=\*)                      # (positive lookahead: doesn't make * part of the match)
+            :@!"'~,#%&-=;<>`            #
+            \.\^\$\+\{\[\]\\\|          #
+        ]*                              #
+        \*/                             # AND */
+     )                                  #
+     | //.*\n                           # OR any character from // until end-line (inclusive)
     """, re.VERBOSE)
 
-    with open(filepath, "r") as file_:
+    # read file and apply regex:
+    with open(input_path, "r") as file_in:
         temp = []
-        for line in file_:
+        for line in file_in:
             temp.append(line)
         output = ''.join(temp)
-        return pattern.sub('', output)
+        output = pattern.sub('', output)
 
+    # write to file:
+    # (`w+` mode: writing/reading; overwrites existing files; creates file if doesn't exit)
+    with open(output_path, "w+") as file_out:
+        file_out.write(output)
 
+#############################
+#           Main            #
+#############################
 if __name__ == "__main__":
-    print(minify('./test/stylesheet.css'))
+    # specify input and output paths in args:
+    minify(args[0], args[1])
