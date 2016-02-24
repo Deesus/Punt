67d78e3d06dbf76d2ad4733eadaa611b637e1241
Dee Reddy <d.reddy@yandex.com>
2016-02-23 21:13:54 -0500

    refactor: Clean and refactor code

--- minify.py
+++ minify.py
@@ -1,11 +1,12 @@
 """
     Minify/uglify CSS files -- i.e. removes spaces and comments from files
+    Specify input and output paths in command line arg:
+    `$ python minify.py ./src/styles.css ./src/output.css`
 
     Copyright 2016 Dee Reddy
 
     TODO:
-        -Add JS option
-        -Better docstrings
+        -Add JS option (detect file type)
 """
 import sys
 import re
@@ -14,33 +15,31 @@ import re
 def minify(input_path, output_path):
     """ Minifies/uglifies file
 
-    Args:
-        input_path:     input file path
-        output_path:    write-out file path
-        comments:       Boolean. If False, deletes comments during output.
-    Returns:
-        Minified string.
-    Example:
-        `$ python minify.py ./src/styles.css ./src/output.css`
+        Args:
+            input_path:     input file path
+            output_path:    write-out file path
+            comments:       Boolean. If False, deletes comments during output.
+        Returns:
+            Minified string.
+        Example:
+            `$ python minify.py ./src/styles.css ./src/output.css`
     """
 
     # matches all in-line and multi-line comments; matches all spaces:
     pattern = re.compile(r"""
     \s |                    # matches all whitespace characters OR
-     (                      # OR any char between /* */ (inclusive)
+    (                       # OR any char between /* */ (inclusive)
         /\*
         (.|\n)*?            # N.b. `*?` performs non-greedy match
         \*/
-     )
-     | //.*\n               # OR any char from // until new-line (inclusive)
+    )
+    | //.*\n                # OR any char from // until new-line (inclusive)
     """, re.VERBOSE)
 
     # read file and apply regex:
     with open(input_path, "r") as file_in:
-        temp = []
-        for line in file_in:
-            temp.append(line)
-        output = ''.join(temp)
+        output = [line for line in file_in]
+        output = ''.join(output)
         output = pattern.sub('', output)
 
     # write to file:
@@ -52,6 +51,5 @@ def minify(input_path, output_path):
 #                 Main               #
 ######################################
 if __name__ == "__main__":
-    # specify input and output paths in command line args:
     args = sys.argv[1:]
     minify(args[0], args[1])
